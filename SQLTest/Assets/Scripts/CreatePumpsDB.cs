5659d440e4353898e81058afd7510531a21999fe
IronGamesCom <39295712+IronGamesCom@users.noreply.github.com>
2018-05-15 14:55:23 +0300

    Revert "Revert "Scene Add Pump Created""
    This reverts commit 50d38a888a3bb39dfdce1659dc5ffc59f487d072.

---
+++ SQLTest/Assets/Scripts/CreatePumpsDB.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CreatePumpsDB : MonoBehaviour
+{
+
+    public Text DebugText;
+    [HideInInspector]
+    public DataService ds;
+    // Use this for initialization
+    void Start()
+    {
+        StartSync();
+    }
+
+    public void StartSync()
+    {
+        ds = new DataService("PumpsDatabase.db");
+        ds.CreatePumpsDB();
+        DebugText.text = "";
+        var pumps = ds.GetPumps();
+        foreach (var item in pumps)
+        {
+            Debug.Log(item.ToString());
+            ToConsole(item.ToString());
+        }
+        
+    }
+
+    private void ToConsole(string msg)
+    {
+        DebugText.text += System.Environment.NewLine + msg;
+        Debug.Log(msg);
+    }
+   
+
+}
+
---
+++ SQLTest/Assets/Scripts/Pumps.cs
@@ -0,0 +1,14 @@
+﻿using SQLite4Unity3d;
+public class Pump { 
+
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Model { get; set; }
+    public long Serial { get; set; }
+    public string MadeIn { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format("[Pump: Id={0}, Model={1},  Serial={2}, MadeIn={3}]", Id, Model, Serial, MadeIn);
+    }
+}
